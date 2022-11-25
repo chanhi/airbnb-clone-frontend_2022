13f97452b68c558b19c6bcc509f03ff9102d31ab
chanhi <61691088+chanhi@users.noreply.github.com>
2022-11-25 20:39:57 +0900

    commit airbnb frontend

---
+++ src/components/Header.tsx
@@ -0,0 +1,110 @@
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  useDisclosure,
+  LightMode,
+  useColorMode,
+  useColorModeValue,
+  Stack,
+  Avatar,
+  Menu,
+  MenuButton,
+  MenuList,
+  useToast,
+  Flex,
+} from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import LoginModal from "./LoginModal";
+import SignUpModal from "./SignUpModal";
+import useUser from "../lib/useUser";
+import { logOut } from "../api";
+import { useQueryClient } from "@tanstack/react-query";
+
+export default function Header() {
+  const { userLoading, isLoggedIn, user } = useUser();
+  const {
+    isOpen: isLoginOpen,
+    onClose: onLoginClose,
+    onOpen: onLoginOpen,
+  } = useDisclosure();
+  const {
+    isOpen: isSignUpOpen,
+    onClose: onSignUpClose,
+    onOpen: onSignUpOpen,
+  } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.200");
+  const Icon = useColorModeValue(FaMoon, FaSun);
+  const toast = useToast();
+  const queryClient = useQueryClient();
+  const onLogout = async () => {
+    const toastId = toast({
+      title: "Login out...",
+      description: "ongoing Log out...",
+      status: "loading",
+      position: "bottom-right",
+    });
+    await logOut();
+    queryClient.refetchQueries(["me"]);
+    toast.update(toastId, {
+      status: "success",
+      title: "Done!",
+      description: "See you later!",
+    });
+  };
+  return (
+    <Stack
+      justifyContent={"space-between"}
+      alignItems="center"
+      py={5}
+      px={40}
+      direction={{
+        sm: "column",
+        md: "row",
+      }}
+      spacing={{
+        sm: 4,
+        md: 0,
+      }}
+      borderBottomWidth={1}
+    >
+      <Box color={logoColor}>
+        <Link to={"/"}>
+          <FaAirbnb size={"48"} />
+        </Link>
+      </Box>
+      <Flex minWidth="max-content" alignItems="center" gap="2">
+        <IconButton
+          onClick={toggleColorMode}
+          variant={"ghost"}
+          aria-label="Toggle dark mode"
+          icon={<Icon />}
+        />
+        {!userLoading ? (
+          !isLoggedIn ? (
+            <>
+              <Button onClick={onLoginOpen}>Log in</Button>
+              <LightMode>
+                <Button onClick={onSignUpOpen} colorScheme={"red"}>
+                  Sign up
+                </Button>
+              </LightMode>
+            </>
+          ) : (
+            <Menu>
+              <MenuButton>
+                <Avatar name={user?.name} src={user?.avatar} size={"md"} />
+              </MenuButton>
+              <MenuList onClick={onLogout}>Log out</MenuList>
+            </Menu>
+          )
+        ) : null}
+      </Flex>
+      <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+      <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
+    </Stack>
+  );
+}
